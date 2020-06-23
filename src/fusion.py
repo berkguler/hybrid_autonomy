7bddf0988010e3f288db28a578830ffb56796ae0
berkguler <gulerb15@itu.edu.tr>
2020-06-23 10:01:34 +0300

    works like a charm

--- src/fusion.py
+++ src/fusion.py
@@ -27,7 +27,9 @@ class TagClass:
 		self.y_box = y_box
 		self.yaw = yaw
 		self.circle = circle
-
+	def display(self):
+		text = ("%s %d %s\nTag:\t%.2f\t%.2f\nBox:\t%.2f\t%.2f\nCircle:\t%.2f\tYaw:\t%.2f"%("--------------------",self.ID,"--------------------",self.x_tag,self.y_tag,self.x_box,self.y_box,self.circle,self.yaw))
+		print(text)
 class TagMatcher:
 
 	def __init__ (self):
@@ -51,22 +53,10 @@ class TagMatcher:
 
 	def explore_n_catch(self):
 		rospy.sleep(0.5)
-		rate = rospy.Rate(5)
 		vision(True)
-		if self.check_readed_markers():
-			self.walk()
-			rate.sleep()
-		else:
-			print self.readedmarkers_tf
-			raw_input('ne yapucuik')
+		self.walk()
 		vision(False)
-		
-	def check_readed_markers(self):
-		# check readedmarkers_tf
-		if len(self.readedmarkers) >= 4:
-			return False
-		else:
-			return True	
+
 
 
 	def laser_classification(self):
@@ -96,28 +86,32 @@ class TagMatcher:
 			rospy.loginfo("Goal Execution Done!")
 
 	def walk(self):
-		waypoints = [(-1,-1.65,0),(3,3.77,3.13),(1,-1.86,3.13),(1,3.05,0),(0,0,0)]
+		waypoints = [(-0.70,-1.65,0),(3,3.70,3.13),(1,-1.86,3.13),(1.45,3.05,0),(0,0,0)]
 		front = 0
 		for point in waypoints:
 			self.random_walk(point)
-			distance = self.laser_classification()
-			front = min(distance['front'],min(distance['fleft'],distance['fright']))-0.2
-			self.moveforward(front)
-			self.moveforward(-front)
-			self.Fusion()
-			yaw = self.getHuskyPose()[3][2]
-			self.rotateccw(yaw+0.45)
-			laser = self.laser_classification()
-			distance = min(min(laser['fleft'],laser['fright']),laser['front'])
-			self.moveforward(front)
-			self.moveforward(-front)
-			self.Fusion()
-			self.rotateccw(yaw-0.45)
-			laser = self.laser_classification()
-			distance = min(min(laser['fleft'],laser['fright']),laser['front'])
-			self.moveforward(front)
-			self.moveforward(-front)
-			self.Fusion()
+			if point != waypoints[len(waypoints)-1]:
+				distance = self.laser_classification()
+				front = min(distance['front'],min(distance['fleft'],distance['fright']))-0.2
+				self.moveforward(front)
+				self.moveforward(-front)
+				self.Fusion()
+				yaw = self.getHuskyPose()[3][2]
+				self.rotateccw(yaw+0.45)
+				laser = self.laser_classification()
+				distance = min(min(laser['fleft'],laser['fright']),laser['front']) 
+				self.moveforward(front)
+				self.moveforward(-front)
+				self.Fusion()
+				self.rotateccw(yaw-0.45)
+				laser = self.laser_classification()
+				distance = min(min(laser['fleft'],laser['fright']),laser['front'])
+				self.moveforward(front)
+				self.moveforward(-front)
+				self.Fusion()
+			else:
+				self.moveforward(4)
+				self.moveforward(-4)
 
 	def calibrate_dance(self):
 		rospy.loginfo("AMCL calibration")
@@ -132,15 +126,32 @@ class TagMatcher:
 			print distance
 			while round(((laser_msg.ranges[360] - initial_range)+distance),1)!= 0:
 				#print("%.2f ---> %.2f"%(-1*distance,(laser_msg.ranges[360] - initial_range)))
+				if distance < 0 and self.is_mf_blocked:
+					self.is_mf_blocked = False
+					break 
 				laser_msg = rospy.wait_for_message("/scan",LaserScan)
 				if min(laser_msg.ranges[144:575]) < 0.1:
+					self.is_mf_blocked = True
 					break
-				speed.linear.x = (distance+(laser_msg.ranges[360] - initial_range))
+				else:
+					self.is_mf_blocked = False
+				linear_speed = (distance+(laser_msg.ranges[360] - initial_range))
+				if distance > 0 and linear_speed < 0:
+					linear_speed = 0
+					self.is_mf_blocked = True
+					break
+				elif distance < 0 and linear_speed > 0:
+					linear_speed = 0
+					self.is_mf_blocked = True
+					break
+				speed.linear.x = linear_speed
 				speed.angular.z = rotation
 				self.move.publish(speed)
 				#print(speed)
 			
 	def rotateccw(self,target_heading):
+		speed = Twist()
+		heading = self.getHuskyPose()[3][2]
 		direction = "ccw"
 		if target_heading > 3.14:
 			target_heading = -3.14 + (target_heading-3.14) 
@@ -148,16 +159,14 @@ class TagMatcher:
 		elif target_heading < -3.14:
 			target_heading = 3.14 + (target_heading + 3.14)
 			direction = "cw"
-		speed = Twist()
-		heading = tf.transformations.euler_from_quaternion(self.getHuskyPose()[1])[2]
-		if (target_heading - heading) >= 0:
+		elif (target_heading - heading) >= 0:
 			direction = "ccw"
-		else:
+		elif (target_heading - heading) < 0:
 			direction = "cw"
-		print("%.2f\t%s"%(target_heading,direction))
+		print("%.2f\t%.2f\t%s"%(heading,target_heading,direction))
 		while not (round(target_heading-heading,1) <= 0.15 and round(target_heading-heading,1) >= -0.15): 
 			#print("%.2f ---> %.2f"%(heading,target_heading))
-			heading = tf.transformations.euler_from_quaternion(self.getHuskyPose()[1])[2]
+			heading = self.getHuskyPose()[3][2]
 			speed.linear.x = 0.0
 			if direction == "ccw":
 				speed.angular.z = 1.0
@@ -175,6 +184,7 @@ class TagMatcher:
 		L_msg, T_msg = self.getMessages()
 		if len(T_msg.markers) >= 1:
 			for i in range(0,len(T_msg.markers)):
+				near_box = (0,0,999,0) #initial box config _x _y _circle _yaw
 				for k in range(0, len(L_msg.tracks)):
 					# Tag
 					self.tag_frame_name = "/ar_marker_" + str(T_msg.markers[i].id)
@@ -188,18 +198,22 @@ class TagMatcher:
 						L_msg.tracks[k].odom.pose.pose.orientation.x,
 						L_msg.tracks[k].odom.pose.pose.orientation.y,
 						L_msg.tracks[k].odom.pose.pose.orientation.z,
-						L_msg.tracks[k].odom.pose.pose.orientation.w
-					)
+						L_msg.tracks[k].odom.pose.pose.orientation.w )
 					box_euler = tf.transformations.euler_from_quaternion(box_orientation)
 					circle = math.sqrt(math.pow(self.tag_trans[0] - box_x,2)+math.pow(self.tag_trans[1] - box_y,2))
-					if circle <= 2 :
-						print("-!-!--!-!---!--!---!--!--!-!--!-!--\nID: %d\nCamera:\t%.2f\t%.2f\nLidar:\t%.2f\t%.2f\nCircle: %.2f\t%.2f"%(T_msg.markers[i].id,self.tag_trans[0],self.tag_trans[1],box_x,box_y,circle,box_euler[2]))
-						if not T_msg.markers[i].id in self.readedmarkers:
-							self.readedmarkers.append(T_msg.markers[i].id)
-						self.readedmarkers_tf.append(TagClass(T_msg.markers[i].id,self.tag_trans[0],self.tag_trans[1],box_x,box_y,box_euler[2],circle))
+					if near_box[2] > circle:
+						near_box = (box_x,box_y,circle,box_euler[2])
+					# if circle < 10:
+					# 	text = ("%s %d %s\nTag:\t%.2f\t%.2f\nBox:\t%.2f\t%.2f\nCircle:\t%.2f\tYaw:\t%.2f"%("--------------------",T_msg.markers[i].id,"--------------------",self.tag_trans[0],self.tag_trans[1],box_x,box_y,circle,box_euler[2]))
+					# 	print(text)
+
+				if not T_msg.markers[i].id in self.readedmarkers:
+					self.readedmarkers.append(T_msg.markers[i].id)
+					rospy.loginfo(str(T_msg.markers[i].id) + " is readed ...")
+				self.readedmarkers_tf.append(TagClass(T_msg.markers[i].id,self.tag_trans[0],self.tag_trans[1],near_box[0],near_box[1],near_box[2],near_box[3]))
 
 	def Rotation(self,translation,orientation):
-		self.rotation_matrix =  np.array([1,0,0,0,0,-1,0,1,0]).reshape(3,3) #-90 degree about the x axis
+		self.rotation_matrix =  np.array([1,0,0,0,0,1,0,-1,0]).reshape(3,3) #-90 degree about the x axis
 		self.converted_trans = np.dot(self.rotation_matrix,np.asarray(translation).reshape(3,1))
 		self.converted_euler = np.dot(self.rotation_matrix,np.asarray(tf.transformations.euler_from_quaternion(orientation)).reshape(3,1))
 		self.converted_quaternion = tf.transformations.quaternion_from_euler(self.converted_euler[0], self.converted_euler[1],self.converted_euler[2])
@@ -209,14 +223,14 @@ class TagMatcher:
 		try:
 			rospy.sleep(1.0)
 			(trans,rot) = self.tf.lookupTransform('/map', '/base_footprint', rospy.Time(0))
-			#print("%s\tHusky\t%s"%("------------","---------------"))
+			print("%s\tHusky\t%s"%("------------","---------------"))
 			homo_matrix = tf.transformations.quaternion_matrix(rot)
 			homo_matrix[0][3] = trans[0]
 			homo_matrix[1][3] = trans[1]
 			homo_matrix[2][3] = trans[2]
-			#print(homo_matrix)
+			print(homo_matrix)
 			yaw = tf.transformations.euler_from_quaternion(rot)
-			#print("%s\tHusky\t%s"%("------------","---------------"))
+			print("%s\tHusky\t%s"%("------------","---------------"))
 			return (trans,rot,homo_matrix,yaw)
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			print("Error Occured! - Detection of Husky Pose wrt map frame")		
@@ -226,27 +240,27 @@ class TagMatcher:
 			(trans,rot) = self.tf.lookupTransform('/base_footprint', tagFrame, rospy.Time(0))
 			new_trans, new_rot = self.Rotation(trans,rot)
 			new_euler = tf.transformations.euler_from_quaternion(new_rot)
-			new_rot = tf.transformations.quaternion_from_euler(0, 0, 0) #new_euler[2]
+			new_rot = tf.transformations.quaternion_from_euler(0, 0, new_euler[2])
 			homo_matrix = tf.transformations.quaternion_matrix(new_rot)
 			homo_matrix[0][3] = new_trans[0]
 			homo_matrix[1][3] = new_trans[1]
 			homo_matrix[2][3] = new_trans[2]
-			# print("%s\tTag\t%s"%("------------","---------------"))
-			# print(homo_matrix)
-			# print("%s\tTag\t%s"%("------------","---------------"))
+			print("%s\tTag\t%s"%("------------","---------------"))
+			print(homo_matrix)
+			print("%s\tTag\t%s"%("------------","---------------"))
 			return (new_trans,new_rot,homo_matrix)
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			print("Error Occured! - Detection of Tag Pose wrt base_footprint frame")	
 		
 	def getTagFrame_wrt_map(self,tagframe):
-		result = self.getHuskyPose()[2] #np.dot(self.getHuskyPose()[2],self.getTagFrame(tagframe)[2])	
-		tag_offset = self.getTagFrame(tagframe)[0]
-		result[0][3] += tag_offset[0]
-		result[1][3] += tag_offset[1]
-		result[2][3] += tag_offset[2]
-		# print("%s\tMapper\t%s"%("------------","---------------"))
-		# print(result)
-		# print("%s\tMapper\t%s"%("------------","---------------"))
+		result = np.dot(self.getHuskyPose()[2],self.getTagFrame(tagframe)[2]) #self.getHuskyPose()[2]	
+		# tag_offset = self.getTagFrame(tagframe)[0]
+		# result[0][3] += tag_offset[0]
+		# result[1][3] += tag_offset[1]
+		# result[2][3] += tag_offset[2]
+		print("%s\tMapper\t%s"%("------------","---------------"))
+		print(result)
+		print("%s\tMapper\t%s"%("------------","---------------"))
 		return result
 
 def movebase_client(target_goal):
@@ -272,9 +286,9 @@ def vision(status):
 		rospy.loginfo("AR Track Alvar is online")
 	else:
 		launch_datmo.shutdown()
-		rospy.loginfo("Datmo is online")
+		rospy.loginfo("Datmo is offline")
 		launch_ar.shutdown()
-		rospy.loginfo("AR Track Alvar is online")
+		rospy.loginfo("AR Track Alvar is offline")
 
 uuid_datmo = roslaunch.rlutil.get_or_generate_uuid(None,False)
 uuid_ar = roslaunch.rlutil.get_or_generate_uuid(None,False)
@@ -292,5 +306,18 @@ if __name__ == '__main__':
 	matcher.move_robot_arm(0,1,0.5,2)
 	matcher.calibrate_dance()
 	matcher.explore_n_catch()
+	for tag_message in matcher.readedmarkers_tf:
+		tag_message.display()	
+	# while True:
+	# 	vision(False)
+	# 	choice = raw_input("Press any key to continue ...")
+	# 	if choice == "f":
+	# 		vision(True)
+	# 		matcher.Fusion()
+	# 	elif choice == "r":	
+	# 		for tag_message in matcher.readedmarkers_tf:
+	# 			tag_message.display()
+	# 	elif choice == "x":
+	# 		break
 	choice2 = raw_input("Press any key to continue ...")
 	matcher.move_robot_arm(0,0,0,0)
